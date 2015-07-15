c2cad38852a918b6db8bd79d4c7f7c276bc653d8
Nick Jüttner <nick.juettner@zalando.de>
2015-07-15 18:18:50 +0200

    only the important function

--- os/loadstat/loadstat_darwin.go
+++ os/loadstat/loadstat_darwin.go
@@ -14,38 +14,8 @@ import (
 	"github.com/square/inspect/os/misc"
 )
 
-// LoadStat represents load average metrics for 1/5/15 Minutes of
-// current operating system.
-// Caution: reflection is used to read this struct to discover names
-// Do not add new types
-type LoadStat struct {
-	OneMinute     *metrics.Gauge
-	FiveMinute    *metrics.Gauge
-	FifteenMinute *metrics.Gauge
-	m             *metrics.MetricContext
-}
-
-// New starts metrics collection every Step and registers with
-// metricscontext
-func New(m *metrics.MetricContext, Step time.Duration) *LoadStat {
-	s := new(LoadStat)
-	s.m = m
-	// initialize all metrics and register them
-	misc.InitializeMetrics(s, m, "loadstat", true)
-	// collect once
-	s.Collect()
-	// collect metrics every Step
-	ticker := time.NewTicker(Step)
-	go func() {
-		for _ = range ticker.C {
-			s.Collect()
-		}
-	}()
-	return s
-}
-
 // Collect populates Loadstat by using sysctl
-func (s *LoadStat) Collect() {
+func (s *LoadStat) CollectDarwin() {
 	cmd := exec.Command("sysctl", "vm.loadavg")
 	tty, err := pty.Start(cmd)
 	if err != nil {
